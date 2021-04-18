0ba7fb65a907ed8925726e48d0a2a0e40093cdb7
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-04-18 09:53:51 -0300

    refactor: fix remaining lint errors

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,13 @@ const theme = {
   },
 };
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
         <Component {...pageProps} />
       </ThemeProvider>
     </>
